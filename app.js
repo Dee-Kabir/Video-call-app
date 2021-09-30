74a730acbb6e000f5a27d3d900ceafd714a3c999
LAPTOP-2NH5B2GR\Deepanshu yadav <kabirsngh0023@gmail.com>
2021-10-01 00:54:35 +0530

    heroku deploy

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const path = require('path');
 const http = require('http')
 const app = express();
-require("dotenv").config();
-const path = require("path");
 const server = http.createServer(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    path: "/socketPath"
+})
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, "client", "build", "index.html")));
 
 io.on("connection",(socket)=>{
     socket.emit("me", socket.id)
     
-    socket.on("disconnect",()=>{
-        socket.broadcast.emit("callEnded")
+    socket.on("disconnectCall",(data)=>{
+        io.to(data.to).emit("callEnded",{message: "call Ended"})
+        io.to(data.from).emit("callEnded",{message: "call Ended"})
     })
 
     socket.on("callUser", (data) => {
@@ -20,20 +26,11 @@ io.on("connection",(socket)=>{
     socket.on("answerCall",(data) => {
         io.to(data.to).emit("callAccepted",data.signal)
     })
-
+    
+    socket.on("busy",data =>{
+        io.to(data.to).emit("busy",{message: data.message})
+    })
 })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
-  } else {
-    app.get("/", (req, res) => {
-      res.send("API Running");
-    });
-  }
-const PORT = process.env.PORT
-app.listen(PORT || 8000, () => {
-    console.log("connected");
-});
+
+server.listen(5000, () => console.log("server is running"))
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import TextField from "@material-ui/core/TextField"
 import AssignmentIcon from "@material-ui/icons/Assignment"
+import MicButton from "@material-ui/icons/Mic"
+import MicOffButton from "@material-ui/icons/MicOff"
+import VideoCamButton from "@material-ui/icons/Videocam"
+import VideoCamOffButton from "@material-ui/icons/VideocamOff"
+import EndCallButton from "@material-ui/icons/PhoneDisabled"
 import PhoneIcon from "@material-ui/icons/Phone"
 import React, { useEffect, useRef, useState } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
@@ -9,7 +14,9 @@ import Peer from "simple-peer"
 import {io} from "socket.io-client"
 import "./App.css"
 
-let socket = io('https://zoomby.herokuapp.com/')
+let socket = io('/',{
+	path: "/socketPath"
+})
 socket = socket.connect()
 function App() {
 	const [ me, setMe ] = useState("")
@@ -21,6 +28,10 @@ function App() {
 	const [ idToCall, setIdToCall ] = useState("")
 	const [ callEnded, setCallEnded] = useState(false)
 	const [ name, setName ] = useState("")
+	const [callerName,setCallerName] = useState("")
+	const [micOn,setMicOn] = useState(true);
+	const [cameraOn,setCameraOn] = useState(true);
+	const [message, setMessage] = useState("");
 	const myVideo = useRef()
 	const userVideo = useRef()
 	const connectionRef= useRef()
@@ -36,10 +47,20 @@ function App() {
 		})
 
 		socket.on("callUser", (data) => {
-			setReceivingCall(true)
-			setCaller(data.from)
-			setName(data.name)
-			setCallerSignal(data.signal)
+			if(receivingCall){
+				socket.emit("busy",{
+					message: "Busy on another call",
+					to : data.from
+			})
+			}else{
+				setReceivingCall(true)
+				setCaller(data.from)
+				setCallerName(data.name)
+				setCallerSignal(data.signal)
+			}
+		})
+		socket.on("callEnded",(data)=>{
+			window.location.reload()
 		})
 	}, [])
 
@@ -49,10 +70,11 @@ function App() {
 			trickle: false,
 			stream: stream
 		})
+		peer.on('error', function (err) { console.log('error', err) })
 		peer.on("signal", (data) => {
 			socket.emit("callUser", {
 				userToCall: id,
-				signalData: data,
+				signalData: cameraOn ? data : "",
 				from: me,
 				name: name
 			})
@@ -63,6 +85,9 @@ function App() {
 		socket.on("callAccepted", (signal) => {
 			setCallAccepted(true)
 			peer.signal(signal)
+		})
+		socket.on("busy",(data)=>{
+			setMessage(data.message)
 		})
 
 		connectionRef.current = peer
@@ -75,6 +100,7 @@ function App() {
 			trickle: false,
 			stream: stream
 		})
+		peer.on('error', function (err) { console.log('error', err) })
 		peer.on("signal", (data) => {
 			socket.emit("answerCall", { signal: data, to: caller })
 		})
@@ -88,68 +114,106 @@ function App() {
 
 	const leaveCall = () => {
 		setCallEnded(true)
+		setCallAccepted(false)
+		socket.emit("disconnectCall",{message: 'disconnect call',to: caller,from:me})
 		connectionRef.current.destroy()
 	}
-
+	function toggleMic() {
+		let enabled = stream.getAudioTracks()[0].enabled;
+		if (enabled) {
+			stream.getAudioTracks()[0].enabled = false;
+			setMicOn(false)
+		} else {
+			setMicOn(true)
+			stream.getAudioTracks()[0].enabled = true;
+		}
+	}
+	function toggleCamera() {
+		let enabled = stream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		stream.getVideoTracks()[0].enabled = false;
+		setCameraOn(false)
+		// userMediaContainers.get("local-user").removeVideo();
+	} else {
+		// document.getElementById('video-logo').innerHTML = room_svg.videoOn;
+		stream.getVideoTracks()[0].enabled = true;
+		setCameraOn(true)
+		// userMediaContainers.get("local-user").addVideo();
+	}
+	}
 	return (
 		<>
 			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
 		<div className="container">
+		{message && <p style={{fontSize: "20px"}}>{message}</p>}
 			<div className="video-container">
 				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+					{stream &&  <div style={{ width: "200px",position: "absolute",zIndex: '10',left: '0px',bottom:'10px' }}>
+						<video playsInline muted ref={myVideo} autoPlay style={{width: '100%',height:'100%'}} />
+						<p style={{backgroundColor: 'black',color: 'white',position:'absolute',right:'0px',bottom:'0px',margin:'0px'}}>{name}</p>
+						</div>}
 				</div>
 				<div className="video">
 					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
+					<div style={{position:'absolute',top:'0px',left:'0px',height:'98vh',width:'100vw'}}>
+					<video playsInline ref={userVideo} autoPlay style={{width: '100%',height:'100%'}}  />
+					<p style={{backgroundColor: 'black',color: 'white',position:'absolute',right:'0px',bottom:'0px',margin:'0px'}}>{callerName}</p>
+					</div>:
 					null}
 				</div>
 			</div>
-			<div className="myId">
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>
+			{!callAccepted && !callEnded ? <div className="myId">
+			<TextField
+				id="filled-basic"
+				label="Name"
+				variant="filled"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				style={{ marginBottom: "20px" }}
+			/>
+			<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+				<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
+					Copy ID
+				</Button>
+			</CopyToClipboard>
 
-				<TextField
-					id="filled-basic"
-					label="ID to call"
-					variant="filled"
-					value={idToCall}
-					onChange={(e) => setIdToCall(e.target.value)}
-				/>
-				<div className="call-button">
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
-						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{idToCall}
-				</div>
+			<TextField
+				id="filled-basic"
+				label="ID to call"
+				variant="filled"
+				value={idToCall}
+				onChange={(e) => setIdToCall(e.target.value)}
+			/>
+			<div className="call-button">
+				
+					<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+						<PhoneIcon fontSize="large" />
+					</IconButton>
+			
+				{idToCall}
 			</div>
+		</div>: null}
 			<div>
 				{receivingCall && !callAccepted ? (
 						<div className="caller">
-						<h1 >{name} is calling...</h1>
+						<h1 >{callerName} is calling...</h1>
 						<Button variant="contained" color="primary" onClick={answerCall}>
 							Answer
 						</Button>
 					</div>
 				) : null}
 			</div>
+			
+			
+			<div style={{position: 'absolute',bottom: '0px',textAlign:'center',width:'100%'}}>
+			<IconButton color="primary" aria-label="mic" onClick={() => toggleMic()}>{micOn ? <MicButton fontSize="large"/> : <MicOffButton fontSize="large" />}</IconButton>
+			<IconButton color="primary" aria-label="mic" onClick={() => toggleCamera()}>{cameraOn ? <VideoCamButton fontSize="large"/> : <VideoCamOffButton fontSize="large" /> }</IconButton>
+			{callAccepted && <IconButton color="primary" aria-label="end call" onClick={leaveCall}>
+			<EndCallButton fontSize="large" />
+		</IconButton>}
+			</div>
+				
+			
 		</div>
 		</>
 	)
